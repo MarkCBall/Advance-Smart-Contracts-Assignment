78e0e695168b680a711357adf3c746f7c98fa5ee
MarkCBall <moball@hotmail.com>
2019-04-03 21:18:30 -0400

    add test info

--- solidity/.solcover.js
+++ solidity/.solcover.js
@@ -1,4 +1,5 @@
 module.exports = {
-    port: 8555,
-    testrpcOptions: '-p 8555 -m "smart fun man behind sea joke split cherry force season once pair"'
+    port: 8545,
+    copyPackages: ['openzeppelin-solidity'],
+    testrpcOptions: '--port 8545 --mnemonic="smart fun man behind sea joke split cherry force season once pair"'
 };
